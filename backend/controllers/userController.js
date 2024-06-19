d8e04bde4a43616a2a1d3cbd250e6d59d8a0f029
tech master <bryan77@twc.com>
2024-06-19 07:54:22 -0400

    setup to register a user and hash password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,7 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/userModel');
 
 // @desc Register a new user 
 // @route /api/users 
@@ -14,7 +17,36 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error("Please include all fields");
     }
 
-    res.send("Register Route");
+    // Find if user already exists
+    const userExists = await User.findOne({email});
+
+    if(userExists) {
+        res.status(400);
+        throw new Error("That user already exists");
+    }
+
+    //Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    //Create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    });
+
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            password: user.password
+        })
+    }
+    else {
+        res.status(400);
+        throw new Error('Invalid user data')
+    }
 });
 
 // @desc Login a user
