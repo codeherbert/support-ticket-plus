45ae58dd2e966a4375b9107410fbf0af52f38fb2
tech master <bryan77@twc.com>
2024-10-18 08:29:02 -0400

    create initial pages and set up routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div>Support Ticket App</div>
+    <>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+      
   )
 }
 
