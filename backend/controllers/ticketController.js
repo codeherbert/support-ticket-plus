29a9e6ab41d1af1d487fdc529ab9985652176843
tech master <bryan77@twc.com>
2026-05-17 00:06:21 -0400

    adds comment to ticketController

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -9,7 +9,8 @@ const Ticket = require('../models/ticketModel');
 const getTickets = asyncHandler(async (req, res) => {
     // Get user using the id in the JWT
     const user = await User.findById(req.user.id);
-
+    
+    // If not user, give error message
     if(!user) {
         res.status(401);
         throw new Error("User not found");
